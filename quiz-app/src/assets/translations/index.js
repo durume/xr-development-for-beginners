4232aeec0a56e2f1d5b587e1634caf219f9a977d
durume <83335854+durume@users.noreply.github.com>
2023-11-25 12:41:28 +0900

    Update index.js to add explanations about Korean

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -1,8 +1,10 @@
 import englishQuizzes from './en/';
 //import spanishQuizzes from './es/';
+//import koreanQuizzes from './ko/';
 
 const messages = {
   en: englishQuizzes,
   //es: spanishQuizzes,
+  //ko: koreanQuizzes,
 };
 export default messages;
